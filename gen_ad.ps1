31f539f498db60864ddcde65a858d315b8e7299d
huyxayda <huxedu2299@gmail.com>
2025-02-11 01:14:58 -0800

    add weaken password policy

--- gen_ad.ps1
+++ gen_ad.ps1
@@ -47,8 +47,18 @@ function CreateADUser(){
     }
 }
 
+#disable password policy to allow weak password user
+function WeakenPasswordPolicy() {
+    secedit /export /cfg C:\Windows\Tasks\secpol.cfg
+    (Get-Content C:\Windows\Tasks\secpol.cfg).replace("PasswordComplexity = 0", "PasswordComplexity = 1").replace("MinimumPasswordLength = 1", "MinimumPasswordLength = 7") | Out-File C:\Windows\Tasks\secpol.cfg
+    secedit /configure /db c:\windows\security\local.sdb /cfg C:\Windows\Tasks\secpol.cfg /areas SECURITYPOLICY
+    rm -force C:\Windows\Tasks\secpol.cfg -confirm:$false
+}
+
 $json = ( Get-Content $JSONFile | ConvertFrom-Json)
 $Global:Domain = $json.domain
+
+WeakenPasswordPolicy
 
 foreach ($group in $json.groups){
     CreateADGroup $group
